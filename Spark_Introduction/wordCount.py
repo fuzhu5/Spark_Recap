5fba43d543a3469d37f1f162aae453cb05d0c347
fuzhu5 <fuzhu@outlook.com.au>
2025-07-30 11:55:50 +1000

    Create wordCount.py

---
+++ Spark_Introduction/wordCount.py
@@ -0,0 +1,20 @@
+"""
+  The file generate the top-5 appeared words in wiki of spark.
+"""
+
+from pyspark import SparkContext
+
+# read file
+textFile = SparkContext().textFile("./wikiOfSpark.txt")
+
+# counting
+wordCount = (
+  textFile.flatMap(lambda line: line.split(" "))
+  .filter(lambda word: word != " ")
+  .map(lambda word: (word, 1))
+  .reduceByKey(lambda x, y: x + y)
+  .sortBy(lambda x: x[1], False)
+  .take(5)
+)
+
+print(wordCount)
